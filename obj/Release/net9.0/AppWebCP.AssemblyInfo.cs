87c7870803cdc67504469ae7ff0acb5967b54a5a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-07 21:49:16 -0300

    :hammer: primeiro commit

--- obj/Release/net9.0/AppWebCP.AssemblyInfo.cs
+++ obj/Release/net9.0/AppWebCP.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppWebCP")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0011722b93ad6ec0bb2e7a099203fdce8e2fd43a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppWebCP")]
